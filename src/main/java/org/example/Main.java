a3e3b0ab912733d80d6a4864ba71d4f61201605b
Igor Różanowski <62307954+Szyrrogi@users.noreply.github.com>
2024-04-10 11:27:29 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        
 
         System.out.println(SimplifiedProbability.calculatePlacesProbabilities(3,4));
-        System.out.println(MonteCarloSimulation.simulate(4,3,100));
+        System.out.println(MonteCarloSimulation.simulate(4,3,100)); // to jest dobrze ;)
     }
 }
