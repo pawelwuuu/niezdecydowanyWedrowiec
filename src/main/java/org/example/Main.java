1e60f426c54dcf6f718dcf241a7a90c39d6253c8
KacperKarbowski <kacperka@poczta.onet.pl>
2024-04-11 20:58:37 +0200

    main update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+
+
         System.out.println(SimplifiedProbability.calculatePlacesProbabilities(3,4));
         System.out.println(MonteCarloSimulation.simulate(4,3,100)); // to jest dobrze ;)
     }
