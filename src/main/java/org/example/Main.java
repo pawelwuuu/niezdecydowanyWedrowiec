3a481b8a4832c4d32f21bfa9afc57810b354af4f
KacperKarbowski <kacperka@poczta.onet.pl>
2024-04-11 20:35:48 +0200

    main update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("fortnite");
+
         System.out.println(SimplifiedProbability.calculatePlacesProbabilities(3,4));
         System.out.println(MonteCarloSimulation.simulate(4,3,100)); // to jest dobrze ;)
     }
