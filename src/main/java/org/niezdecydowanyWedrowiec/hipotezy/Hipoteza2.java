e750ce08c670f6599ebe8e7f299a394bd05ee463
SzefuKox123PL <szyrrogi@hotmail.com>
2024-05-04 19:54:34 +0200

    H2

--- src/main/java/org/niezdecydowanyWedrowiec/hipotezy/Hipoteza2.java
+++ src/main/java/org/niezdecydowanyWedrowiec/hipotezy/Hipoteza2.java
@@ -11,10 +11,10 @@ import org.niezdecydowanyWedrowiec.prawodpodobienstwa.WersjaPodstawowaSymulacja;
 
 public class Hipoteza2 {
     double[] rozwiaz = GaussSeidel.rozwiaz(WersjaPodstawowaMacierz.macierzPodstawowa(), WersjaPodstawowaMacierz.podajWektorWynikowy(), 10000, 1e-20);
-    //double[] rozwiaz1 = GaussZCzesciowymWyborem.rozwiaz(WersjaPodstawowaMacierz.macierzPodstawowa(), WersjaPodstawowaMacierz.podajWektorWynikowy());
+    double[] rozwiaz1 = GaussZCzesciowymWyborem.rozwiaz(WersjaPodstawowaMacierz.macierzPodstawowa(), WersjaPodstawowaMacierz.podajWektorWynikowy());
     //double[] rozwiaz = GaussEliminacja.rozwiaz(WersjaPodstawowaMacierz.macierzPodstawowa(), WersjaPodstawowaMacierz.podajWektorWynikowy());
     double suma1;
-    double sumMonte;
+    double sumaA2;
     double sumaA3;
     //
 //    public void obliczSume() {
@@ -34,114 +34,73 @@ public class Hipoteza2 {
 
         System.out.println("MM");
         for(int i = 0; i < rozwiaz.length; i++) {
-            double Monte = WersjaPodstawowaSymulacja.symuluj(100000,i+1);
-            suma1 += Math.abs(rozwiaz[i] - Monte);
-            sumMonte += Monte;
+            suma1 += Math.abs(rozwiaz[i] - rozwiaz1[i]);
+            sumaA2 += Math.abs(rozwiaz1[i]);
             sumaA3 += Math.abs(rozwiaz[i]);
             //System.out.println(rozwiaz[i] + " " + WersjaPodstawowaSymulacja.symuluj(1000,i+1));
         }
-        System.out.println(sumMonte + " " + sumaA3 + " " + suma1);
+        System.out.println(sumaA2 + " " + sumaA3 + " " + suma1);
 
 
         suma1 = 0;
-        sumMonte = 0;
+        sumaA2 = 0;
         sumaA3 = 0;
 
         System.out.println();
         Main.nazwaPliku = "src\\main\\java\\org\\niezdecydowanyWedrowiec\\parki\\parkMD0.txt";
         rozwiaz = GaussSeidel.rozwiaz(WersjaPodstawowaMacierz.macierzPodstawowa(), WersjaPodstawowaMacierz.podajWektorWynikowy(), 10000, 1e-20);
-
+        rozwiaz1 = GaussZCzesciowymWyborem.rozwiaz(WersjaPodstawowaMacierz.macierzPodstawowa(), WersjaPodstawowaMacierz.podajWektorWynikowy());
 
         System.out.println("MD");
         for(int i = 0; i < rozwiaz.length; i++) {
-            double Monte = WersjaPodstawowaSymulacja.symuluj(100000,i+1);
-            suma1 += Math.abs(rozwiaz[i] - Monte);
-            sumMonte += Monte;
+            suma1 += Math.abs(rozwiaz[i] - rozwiaz1[i]);
+            sumaA2 += Math.abs(rozwiaz1[i]);
             sumaA3 += Math.abs(rozwiaz[i]);
             //System.out.println(rozwiaz[i] + " " + WersjaPodstawowaSymulacja.symuluj(1000,i+1));
         }
-        System.out.println(sumMonte + " " + sumaA3 + " " + suma1);
+        System.out.println(sumaA2 + " " + sumaA3 + " " + suma1);
 
 
         suma1 = 0;
-        sumMonte = 0;
+        sumaA2 = 0;
         sumaA3 = 0;
 
         System.out.println();
         Main.nazwaPliku = "src\\main\\java\\org\\niezdecydowanyWedrowiec\\parki\\parkDM0.txt";
         rozwiaz = GaussSeidel.rozwiaz(WersjaPodstawowaMacierz.macierzPodstawowa(), WersjaPodstawowaMacierz.podajWektorWynikowy(), 10000, 1e-20);
+        rozwiaz1 = GaussZCzesciowymWyborem.rozwiaz(WersjaPodstawowaMacierz.macierzPodstawowa(), WersjaPodstawowaMacierz.podajWektorWynikowy());
 
         System.out.println("DM");
         for(int i = 0; i < rozwiaz.length; i++) {
-            double Monte = WersjaPodstawowaSymulacja.symuluj(10000,i+1);
-            suma1 += Math.abs(rozwiaz[i] - Monte);
-            sumMonte += Monte;
+            suma1 += Math.abs(rozwiaz[i] - rozwiaz1[i]);
+            sumaA2 += Math.abs(rozwiaz1[i]);
             sumaA3 += Math.abs(rozwiaz[i]);
             //System.out.println(rozwiaz[i] + " " + WersjaPodstawowaSymulacja.symuluj(1000,i+1));
         }
-        System.out.println(sumMonte + " " + sumaA3 + " " + suma1);
+        System.out.println(sumaA2 + " " + sumaA3 + " " + suma1);
 
 
         suma1 = 0;
-        sumMonte = 0;
+        sumaA2 = 0;
         sumaA3 = 0;
 
         System.out.println();
         Main.nazwaPliku = "src\\main\\java\\org\\niezdecydowanyWedrowiec\\parki\\parkDD0.txt";
         rozwiaz = GaussSeidel.rozwiaz(WersjaPodstawowaMacierz.macierzPodstawowa(), WersjaPodstawowaMacierz.podajWektorWynikowy(), 10000, 1e-20);
-
+        rozwiaz1 = GaussZCzesciowymWyborem.rozwiaz(WersjaPodstawowaMacierz.macierzPodstawowa(), WersjaPodstawowaMacierz.podajWektorWynikowy());
 
         System.out.println("DD");
         for(int i = 0; i < rozwiaz.length; i++) {
-            double Monte = WersjaPodstawowaSymulacja.symuluj(10000,i+1);
-            suma1 += Math.abs(rozwiaz[i] - Monte);
-            sumMonte += Monte;
+            suma1 += Math.abs(rozwiaz[i] - rozwiaz1[i]);
+            sumaA2 += Math.abs(rozwiaz1[i]);
             sumaA3 += Math.abs(rozwiaz[i]);
             //System.out.println(rozwiaz[i] + " " + WersjaPodstawowaSymulacja.symuluj(1000,i+1));
         }
-        System.out.println(sumMonte + " " + sumaA3 + " " + suma1);
+        System.out.println(sumaA2 + " " + sumaA3 + " " + suma1);
 
 
         System.out.println();
 
     }
-//        double[] rozwiaz = GaussSeidel.rozwiaz(new MacierzRzadka(WersjaPodstawowaMacierz.macierzPodstawowa()), WersjaPodstawowaMacierz.podajWektorWynikowy(), 10000, 1e-20);
-//        double[] rozwiaz1 = GaussEliminacja.rozwiaz(new MacierzRzadka(WersjaPodstawowaMacierz.macierzPodstawowa()), WersjaPodstawowaMacierz.podajWektorWynikowy());
-//
-
-//        obliczSume();
-//       // System.out.println(suma1 - suma2);
-//        System.out.println(WersjaPodstawowaSymulacja.symuluj(1000000,0));
-//        //System.out.println(rozwiaz1[0] - WersjaPodstawowaSymulacja.symuluj(1000,0));
-//        }
-//
-//        Main.nazwaPliku = "src\\main\\java\\org\\niezdecydowanyWedrowiec\\parki\\parkDM0.txt";
-//        rozwiaz = GaussSeidel.rozwiaz(new MacierzRzadka(WersjaPodstawowaMacierz.macierzPodstawowa()), WersjaPodstawowaMacierz.podajWektorWynikowy(), 10000, 1e-20);
-//        rozwiaz1 = GaussEliminacja.rozwiaz(new MacierzRzadka(WersjaPodstawowaMacierz.macierzPodstawowa()), WersjaPodstawowaMacierz.podajWektorWynikowy());
-//
-//
-//        System.out.println("gauss - gaussZwyborem");
-//        for (int i = 0; i < rozwiaz.length; i++) {
-//            System.out.println(rozwiaz[i] - rozwiaz1[i]);
-//        }
-//        Main.nazwaPliku = "src\\main\\java\\org\\niezdecydowanyWedrowiec\\parki\\parkMM0.txt";
-//        rozwiaz = GaussSeidel.rozwiaz(new MacierzRzadka(WersjaPodstawowaMacierz.macierzPodstawowa()), WersjaPodstawowaMacierz.podajWektorWynikowy(), 10000, 1e-20);
-//        rozwiaz1 = GaussEliminacja.rozwiaz(new MacierzRzadka(WersjaPodstawowaMacierz.macierzPodstawowa()), WersjaPodstawowaMacierz.podajWektorWynikowy());
-//
-//
-//        System.out.println("gauss - gaussZwyborem");
-//        for (int i = 0; i < rozwiaz.length; i++) {
-//            System.out.println(rozwiaz[i] - rozwiaz1[i]);
-//        }
-//        Main.nazwaPliku = "src\\main\\java\\org\\niezdecydowanyWedrowiec\\parki\\parkMD0.txt";
-//        rozwiaz = GaussSeidel.rozwiaz(new MacierzRzadka(WersjaPodstawowaMacierz.macierzPodstawowa()), WersjaPodstawowaMacierz.podajWektorWynikowy(), 10000, 1e-20);
-//        rozwiaz1 = GaussEliminacja.rozwiaz(new MacierzRzadka(WersjaPodstawowaMacierz.macierzPodstawowa()), WersjaPodstawowaMacierz.podajWektorWynikowy());
-//
-//
-//        System.out.println("gauss - gaussZwyborem");
-//        for (int i = 0; i < rozwiaz.length; i++) {
-//            System.out.println(rozwiaz[i] - rozwiaz1[i]);
-
-
 
 }
